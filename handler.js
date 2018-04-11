4b5a76c10cfc1cd2ea86b6bf9d84699b0dd18664
Rafael Furry <rfurry@gmail.com>
2018-04-11 05:44:26 -0700

    Create legislators function

--- handler.js
+++ handler.js
@@ -1,16 +1,13 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.legislators = (event, context, callback) => {
+  
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: JSON.stringify()
     }),
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
